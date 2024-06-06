ea0b61511ebd2ea0877360e0214cb0637535a7db
nexxod <duxyoqv0x@mozmail.com>
2024-06-06 21:45:33 +0200

    Folder changes

--- ArcadiaMoonPlugin/obj/Release/netstandard2.1/ArcadiaMoonPlugin.AssemblyInfo.cs
+++ ArcadiaMoonPlugin/obj/Release/netstandard2.1/ArcadiaMoonPlugin.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("A helper tool for ArcadiaMoon mod")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43206f0475d7dae18e393eaa115cfd192399712f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ede3355d055c1cddf0053f4590d5942209f8ba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArcadiaMoonPlugin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArcadiaMoonPlugin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
